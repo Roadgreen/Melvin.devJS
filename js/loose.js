d5cdc66176b56c9c4019fccdfa076933975314c0
Melvin.B <41331833+Roadgreen@users.noreply.github.com>
2021-07-02 16:29:59 +0200

    first commit

--- js/loose.js
+++ js/loose.js
@@ -1,2 +1,15 @@
 var audio = new Audio('/../song/harrymomentum.mp3');
 audio.play();
+
+
+
+this.sound = new Audio();
+this.sound.src = '/../song/harrymomentum.mp3';
+this.sound.load();
+this.sound.play()
+  .then(() => {
+    // Audio is playing.
+  })
+  .catch(error => {
+    console.log(error);
+  });
