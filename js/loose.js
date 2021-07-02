ea5313f2c6ee9dcf92b1e8271a04f4f958a863c1
Melvin.B <41331833+Roadgreen@users.noreply.github.com>
2021-07-02 16:39:55 +0200

    first commit

--- js/loose.js
+++ js/loose.js
@@ -4,7 +4,7 @@ audio.play();
 
 
 this.sound = new Audio();
-this.sound.src = '../img/harrymomentum.mp3';
+this.sound.src = 'https://raw.githubusercontent.com/Roadgreen/Melvin.devJS/main/img/harrymomentum.mp3';
 this.sound.load();
 this.sound.play()
   .then(() => {
